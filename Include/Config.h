2808c2542e9d19dcd0dd0d8bebcec6ab13b3217d
huweiqing <huweiqing915@gmail.com>
2014-05-12 16:29:22 +0800

    update

--- Include/Config.h
+++ Include/Config.h
@@ -19,7 +19,7 @@
 #include "MutexLock.h"
 #include "Log.h"
 
-#define CONFIG_DAT_PATH "/var/www/spell_correct/Conf/config.dat" //配置文件存放的位置
+#define CONFIG_DAT_PATH "../Conf/config.dat" //配置文件存放的位置
 #define MATCH_CHARACTER '='  //配置文件的格式，如server_port = 6688
 
 class Config {
